be108f5122cb19d1976c842357706f5ccd00a778
saul <sauld@cimat.mx>
2023-07-20 16:07:51 -0700

    workflow for testing fixed

--- src/functions.jl
+++ src/functions.jl
@@ -1,3 +1,5 @@
 function greet_vaccine_stock_management_with_mdps()
-    println("Hello VaccineStockManagementWithMDPs")
+    str = "Hello VaccineStockManagementWithMDPs!"
+    println(str)
+    return str
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using VaccineStockManagementWithMDPs
 using Test
 
 @testset "VaccineStockManagementWithMDPs.jl" begin
-    @test VaccineStockManagementWithMDPs.greet_vaccine_stock_management_with_mdps() == "Hello VaccineStockManagementWithMDPs"
+    @test VaccineStockManagementWithMDPs.greet_vaccine_stock_management_with_mdps() == "Hello VaccineStockManagementWithMDPs!"
+    @test VaccineStockManagementWithMDPs.greet_vaccine_stock_management_with_mdps() != "Hellow World!"
 end
