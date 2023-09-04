8c01283e14c8d6384ea19cf2cd513238eb558fbc
= <=>
2023-09-04 09:25:23 -0700

    with load_parameters tested

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using VaccineStockManagementWithMDPs
 using Test
 
 @testset "VaccineStockManagementWithMDPs.jl" begin
-    # Write your tests here.
+    @test VaccineStockManagementWithMDPs.greet_vaccine_stock_management_with_mdps() == "Hello VaccineStockManagementWithMDPs!"
+    @test VaccineStockManagementWithMDPs.greet_vaccine_stock_management_with_mdps() != "Hellow World!"
+    VaccineStockManagementWithMDPs.load_parameters()
+    @test VaccineStockManagementWithMDPs.load_parameters().N_grid_size[1] == 500
 end
