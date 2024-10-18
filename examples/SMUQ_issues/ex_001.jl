29e68bfd8f2c44ad89df086c573704b4a5856083
SAUL DIAZ INFANTE VELASCO <sauld@cimat.mx>
2024-10-18 09:38:56 -0700

    new tree structure

---
+++ examples/SMUQ_issues/ex_001.jl
@@ -0,0 +1,13 @@
+using VaccineStockManagementWithMDPs
+using Test
+using DataFrames, CSV
+using CairoMakie
+CairoMakie.activate!()
+#
+sampling_size = 1000;
+df_par, df_mc, path_par, path_mc = montecarlo_sampling(
+    sampling_size,
+    "data/parameters_model.json"
+)
+df_stat = get_simulation_statistics()
+
